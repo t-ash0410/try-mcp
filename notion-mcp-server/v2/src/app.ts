075e68840db7be6de94e6ea300bad43c5e58983f
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-19 22:40:27 +0900

    apply information log

--- notion-mcp-server/v2/src/app.ts
+++ notion-mcp-server/v2/src/app.ts
@@ -1,8 +1,16 @@
 import { Hono } from 'hono'
+import { createMiddleware } from 'hono/factory'
 
 const app = new Hono()
   // .use(initContext)
-  // .use('*', informationLog)
+  .use('*', createMiddleware(async (c, next) => {
+    console.log({
+      message: `Request received ${c.req.url}`,
+      headers: c.req.header(),
+      body: await c.req.text(),
+    })
+    await next()
+  }))
   // .onError(errorHandler)
   .get('/health', (c) => {
     return c.json({
